4249ff8fa189f77358eef31aac7e29cd321b1c86
mpetavy <mpetavy>
2025-04-13 19:31:49 +0200

    i initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"embed"
+	"github.com/mpetavy/common"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+//go:embed go.mod
+var resources embed.FS
+
+const (
+	BookmarkFile = "bookmarks_3_24_25.html"
+)
+
+func init() {
+	common.Init("", "", "", "", "", "", "", "", &resources, nil, nil, run, 0)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	common.DebugFunc()
+
+	sb, err := func() (*common.SwapBuffer, error) {
+		sb := common.NewSwapBuffer()
+
+		ba, err := os.ReadFile(BookmarkFile)
+		if common.Error(err) {
+			return nil, err
+		}
+
+		_, err = sb.Write(ba)
+		if common.Error(err) {
+			return nil, err
+		}
+
+		w.Header().Set(common.CONTENT_TYPE, common.MimetypeApplicationJson.MimeType)
+		w.Header().Set(common.CONTENT_LENGTH, strconv.Itoa(sb.Len()))
+
+		return sb, nil
+	}()
+
+	switch err {
+	case nil:
+		common.Error(common.HTTPResponse(w, r, http.StatusOK, common.MimetypeApplicationJson.MimeType, sb.Len(), sb))
+	default:
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+func setHandler(w http.ResponseWriter, r *http.Request) {
+	common.DebugFunc()
+
+}
+
+func run() error {
+	http.HandleFunc("/get", getHandler)
+	http.HandleFunc("/set", setHandler)
+
+	err := http.ListenAndServe(":6666", nil)
+	if common.Error(err) {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	common.Run(nil)
+}
